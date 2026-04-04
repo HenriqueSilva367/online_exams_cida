649f45d01fa3200fe618af959d993d303c168148
Henrique Silva <jhenrique367@gmail.com>
2026-04-04 14:02:25 -0300

    Fead: add new exam and questions

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,5 +2,8 @@ class Exam < ApplicationRecord
   belongs_to :topic
   has_many :questions, dependent: :destroy
   has_many :exam_sessions, dependent: :destroy
+  
+  enum :exam_type, { normal: 0, anac_pp: 1 }
+
   validates :title, :time_duration, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :exam
+  belongs_to :topic, optional: true
   has_many :answers, dependent: :destroy
   validates :title, presence: true
 end
